6a3594d53b5b7c091cb6319859110cbd6403487b
alanxyz <67667676+alanxyz@users.noreply.github.com>
2020-08-30 22:38:10 +0100

    Update mailrm.sh

--- mailrm.sh
+++ mailrm.sh
@@ -2,7 +2,7 @@
 
 # Purge all configs from a previous attempt at a mail server.
 # Doesn't delete mail or anything like that.
-pacman -Rcsn dovecot spamassassin postfix spamasssassin opendkim
+pacman -Rcsn dovecot spamassassin postfix spamassassin opendkim
 
 # Some stragglers that often stay undeleted.
 rm -rf /etc/dovecot /var/lib/dovecot
