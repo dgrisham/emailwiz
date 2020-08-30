f0f092bdbe01ea5861ab45442a9efb3b4c868dfe
alanxyz <67667676+alanxyz@users.noreply.github.com>
2020-08-30 21:32:57 +0100

    Update mailrm.sh
    changed apt purge to pacman -Rcsn

--- mailrm.sh
+++ mailrm.sh
@@ -2,7 +2,7 @@
 
 # Purge all configs from a previous attempt at a mail server.
 # Doesn't delete mail or anything like that.
-apt purge dovecot-core spamassassin postfix spamc opendkim
+pacman -Rcsn dovecot-core spamassassin postfix spamc opendkim
 
 # Some stragglers that often stay undeleted.
 rm -rf /etc/dovecot /var/lib/dovecot
